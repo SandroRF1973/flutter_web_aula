e28ec896e6d3cb5cd8ef8d0d0a5155f30de56471
SandroRF <sandrorf1973@gmail.com>
2023-05-02 16:04:12 -0300

    Aula - 300

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_aula/responsividade_media_query.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Flutter Web",
+    debugShowCheckedModeBanner: false,
+    home: ResponsividadeMediaQuery(),
+  ));
+}
---
+++ lib/responsividade_media_query.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ResponsividadeMediaQuery extends StatefulWidget {
+  const ResponsividadeMediaQuery({super.key});
+
+  @override
+  State<ResponsividadeMediaQuery> createState() =>
+      _ResponsividadeMediaQueryState();
+}
+
+class _ResponsividadeMediaQueryState extends State<ResponsividadeMediaQuery> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
