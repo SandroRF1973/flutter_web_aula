4ee0ef4cbc9e2f47f02a9587a92ee8397ee1712a
SandroRF <sandrorf1973@gmail.com>
2023-05-03 09:39:18 -0300

    Aula - 301

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_aula/responsividade_media_query.dart';
+import 'package:flutter_web_aula/responsividade_row_col.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: "Flutter Web",
     debugShowCheckedModeBanner: false,
-    home: ResponsividadeMediaQuery(),
+    // home: ResponsividadeMediaQuery(),
+    home: ResponsividadeRowCol(),
   ));
 }
