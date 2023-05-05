4839a2f3449059c8f40688f817ecf638dd714472
SandroRF <sandrorf1973@gmail.com>
2023-05-05 10:52:20 -0300

    Aula - 306

--- lib/loja_virtual.dart
+++ lib/loja_virtual.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_aula/widget/item_produto.dart';
 import 'package:flutter_web_aula/widget/mobile_app_bar.dart';
 import 'package:flutter_web_aula/widget/web_app_bar.dart';
 
@@ -10,6 +11,20 @@ class LojaVirtual extends StatefulWidget {
 }
 
 class _LojaVirtualState extends State<LojaVirtual> {
+  _ajustarVisualizacao(double larguraTela) {
+    int colunas = 2;
+
+    if (larguraTela <= 600) {
+      colunas = 2;
+    } else if (larguraTela <= 960) {
+      colunas = 4;
+    } else {
+      colunas = 6;
+    }
+
+    return colunas;
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraint) {
@@ -26,6 +41,22 @@ class _LojaVirtualState extends State<LojaVirtual> {
                 preferredSize: Size(largura, alturaBarra),
                 child: const WebAppBar(),
               ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: GridView.count(
+            crossAxisCount: _ajustarVisualizacao(largura),
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+            children: [
+              ItemProduto("Kit Multimídia", "2.500,00", "p1.jpg"),
+              ItemProduto("Pneu Goodyear", "800,00", "p2.jpg"),
+              ItemProduto("Samsumg 9", "4.100,00", "p3.jpg"),
+              ItemProduto("Samsumg Edge", "2.150,90", "p4.jpg"),
+              ItemProduto("Galaxy 10", "6.200,00", "p5.jpg"),
+              ItemProduto("Iphone 10", "1.900,20", "p6.jpg"),
+            ],
+          ),
+        ),
       );
     });
   }
